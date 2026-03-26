fe42d607f2a4be3fb37f855256ae9e7f3322bbdf
Wannes Baes <wabaes@b101.lxd>
2026-03-26 17:51:12 +0100

    sim results ZF + LSV

--- mu-mimo/main.py
+++ mu-mimo/main.py
@@ -50,11 +50,6 @@ if __name__ == "__main__":
     # CHOOSE THE SIMULATION AND SYSTEM CONFIGURATIONS HERE.
     sim_ref_numbers = ["1.0"]
     sys_ref_numbers = [f"1.{i}.{j}.{k}" for i in range(1,4) for j in range(1, 5) for k in range(1, 4)]
-
-    sys_ref_numbers_ZF_LSV_4QAM = ["1.2.1.1", "1.2.1.2", "1.2.1.3"]
-    sys_ref_numbers_ZF_LSV_64QAM = ["1.2.2.1", "1.2.2.2", "1.2.2.3"]
-    sys_ref_numbers_ZF_LSV_100RQAM = ["1.2.3.1", "1.2.3.2", "1.2.3.3"]
-    sys_ref_numbers_ZF_LSV_75RQAM = ["1.2.3.1", "1.2.3.2", "1.2.3.3"]
     
     # RUN OR LOAD YOUR SIMULATIONS HERE.
     results = main(sim_ref_numbers, sys_ref_numbers)
