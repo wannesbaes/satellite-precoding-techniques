20627b145ba21134132a505b59af48015ca39a58
Wannes Baes <wabaes@b101.lxd>
2026-05-25 22:00:58 +0200

    sims6

--- mu-mimo/main.py
+++ mu-mimo/main.py
@@ -56,7 +56,7 @@ if __name__ == "__main__":
     results = main(sim_ref_numbers, system_ref_numbers)
     
     # PLOT THE RESULTS HERE.
-    SimResultManager.plot_system_performance_comparison(results, label_type="RTT", ibr=False, ber=False)
+    # SimResultManager.plot_system_performance_comparison(results, label_type="RTT", ibr=False, ber=False)
 
     
     
