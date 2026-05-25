3a560c3c6d2b310abb05d0a5fe40945e16051801
wabaes <wannes.baes@ugent.be>
2026-05-25 23:56:46 +0200

    setup sims 9

--- mu-mimo/main.py
+++ mu-mimo/main.py
@@ -49,12 +49,25 @@ if __name__ == "__main__":
     sim_ref_numbers = ["1.test"]
     system_ref_numbers = [
         "1_sl_0.1.5.3", "1_sl_0.3.5.3", "1_sl_0.4.5.3",
+
         "2_sl_1on4.1.5.3", "2_sl_2on4.1.5.3", "2_sl_3on4.1.5.3", "2_sl_4on4.1.5.3", "2_sl_5on4.1.5.3", "2_sl_6on4.1.5.3", "2_sl_7on4.1.5.3", "2_sl_8on4.1.5.3",
         "2_sl_1on4.3.5.3", "2_sl_2on4.3.5.3", "2_sl_3on4.3.5.3", "2_sl_4on4.3.5.3", "2_sl_5on4.3.5.3", "2_sl_6on4.3.5.3", "2_sl_7on4.3.5.3", "2_sl_8on4.3.5.3",
         "2_sl_1on4.4.5.3", "2_sl_2on4.4.5.3", "2_sl_3on4.4.5.3", "2_sl_4on4.4.5.3", "2_sl_5on4.4.5.3", "2_sl_6on4.4.5.3", "2_sl_7on4.4.5.3", "2_sl_8on4.4.5.3",
+
         "3_sl_1on4.1.5.3", "3_sl_2on4.1.5.3", "3_sl_3on4.1.5.3", "3_sl_4on4.1.5.3", "3_sl_5on4.1.5.3", "3_sl_6on4.1.5.3", "3_sl_7on4.1.5.3", "3_sl_8on4.1.5.3",
         "3_sl_1on4.3.5.3", "3_sl_2on4.3.5.3", "3_sl_3on4.3.5.3", "3_sl_4on4.3.5.3", "3_sl_5on4.3.5.3", "3_sl_6on4.3.5.3", "3_sl_7on4.3.5.3", "3_sl_8on4.3.5.3",
         "3_sl_1on4.4.5.3", "3_sl_2on4.4.5.3", "3_sl_3on4.4.5.3", "3_sl_4on4.4.5.3", "3_sl_5on4.4.5.3", "3_sl_6on4.4.5.3", "3_sl_7on4.4.5.3", "3_sl_8on4.4.5.3",
+
+
+        "1s_sl_0.1.5.4", "1s_sl_0.3.5.4", "1s_sl_0.4.5.4",
+
+        "2s_sl_1on4.1.5.4", "2s_sl_2on4.1.5.4", "2s_sl_3on4.1.5.4", "2s_sl_4on4.1.5.4", "2s_sl_5on4.1.5.4", "2s_sl_6on4.1.5.4", "2s_sl_7on4.1.5.4", "2s_sl_8on4.1.5.4",
+        "2s_sl_1on4.3.5.4", "2s_sl_2on4.3.5.4", "2s_sl_3on4.3.5.4", "2s_sl_4on4.3.5.4", "2s_sl_5on4.3.5.4", "2s_sl_6on4.3.5.4", "2s_sl_7on4.3.5.4", "2s_sl_8on4.3.5.4",
+        "2s_sl_1on4.4.5.4", "2s_sl_2on4.4.5.4", "2s_sl_3on4.4.5.4", "2s_sl_4on4.4.5.4", "2s_sl_5on4.4.5.4", "2s_sl_6on4.4.5.4", "2s_sl_7on4.4.5.4", "2s_sl_8on4.4.5.4",
+
+        "3s_sl_1on4.1.5.4", "3s_sl_2on4.1.5.4", "3s_sl_3on4.1.5.4", "3s_sl_4on4.1.5.4", "3s_sl_5on4.1.5.4", "3s_sl_6on4.1.5.4", "3s_sl_7on4.1.5.4", "3s_sl_8on4.1.5.4",
+        "3s_sl_1on4.3.5.4", "3s_sl_2on4.3.5.4", "3s_sl_3on4.3.5.4", "3s_sl_4on4.3.5.4", "3s_sl_5on4.3.5.4", "3s_sl_6on4.3.5.4", "3s_sl_7on4.3.5.4", "3s_sl_8on4.3.5.4",
+        "3s_sl_1on4.4.5.4", "3s_sl_2on4.4.5.4", "3s_sl_3on4.4.5.4", "3s_sl_4on4.4.5.4", "3s_sl_5on4.4.5.4", "3s_sl_6on4.4.5.4", "3s_sl_7on4.4.5.4", "3s_sl_8on4.4.5.4",
     ]
 
     # RUN OR LOAD YOUR SIMULATIONS HERE.
@@ -62,26 +75,6 @@ if __name__ == "__main__":
     
     # PLOT THE RESULTS HERE.
     # SimResultManager.plot_system_performance_comparison(results, label_type="RTT", ibr=False, ber=False)
-    
-    
-    
-    
-    # a = [
-    #     ["1_sl_0.4.5.3", "2_sl_2on4.4.5.3", "3_sl_2on4.4.5.3"],
-    #     ["1_sl_0.4.5.3", "2_sl_3on4.4.5.3", "3_sl_3on4.4.5.3"],
-    #     ["1_sl_0.4.5.3", "2_sl_4on4.4.5.3", "3_sl_4on4.4.5.3"],
-    #     ["1_sl_0.4.5.3", "2_sl_5on4.4.5.3", "3_sl_5on4.4.5.3"],
-    #     ["1_sl_0.4.5.3", "2_sl_6on4.4.5.3", "3_sl_6on4.4.5.3"],
-    #     ["1_sl_0.4.5.3", "2_sl_7on4.4.5.3", "3_sl_7on4.4.5.3"],
-    #     ["1_sl_0.4.5.3", "2_sl_8on4.4.5.3", "3_sl_8on4.4.5.3"]
-    # ]
-    # for system_ref_number in system_ref_numbers:
-
-    #     # RUN OR LOAD YOUR SIMULATIONS HERE.
-    #     results = main(sim_ref_numbers, system_ref_number)
-        
-    #     # PLOT THE RESULTS HERE.
-    #     SimResultManager.plot_system_performance_comparison(results, label_type="CE", ibr=False, ber=False)
 
     
     
